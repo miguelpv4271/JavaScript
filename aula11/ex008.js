178acb923134ada789ce21abe0aa8582331d8bb3
miguelpv4271 <miguelpaivav@gmail.com>
2025-04-27 14:10:06 -0300

    ex 08 até 10

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60){ //Condição simples (sem senão)
+    console.log(`Você ultrapassou a velocidade permitida, multado!`)
+}
+
+console.log('Dirija sempre usando sinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Botswana'
+console.log(`Vivendo em ${país}, você é`)
+if (país == "Brasil"){
+    console.log("Bostileiro")
+}else{
+    console.log("Estrangeiro")
+}
