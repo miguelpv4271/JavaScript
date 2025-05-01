2d66da0e92a12c95c5c13727c4488999d36cb96a
miguelpv4271 <miguelpaivav@gmail.com>
2025-05-01 19:41:25 -0300

    aula 12

---
+++ aula12/ex012.js
@@ -0,0 +1,14 @@
+var agora = new Date()
+var hora = agora.getHours()
+
+console.log(`Agora são exatamente ${hora} horas`)
+
+if (hora < 5){
+    console.log(`Boa madrugada! Vai dormir`)
+} else if (hora < 12){
+    console.log(`Bom dia! Será que é um dia a mais ou um dia a menos?`)
+} else if (hora < 19){
+    console.log(`Boa tarde! Neymar`)
+} else {
+    console.log(`Boa noite! Bruno`)
+}
