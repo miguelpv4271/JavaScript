752aaaf76fa7c1a28e03d63b5b4005d1517b5112
miguelpv4271 <miguelpaivav@gmail.com>
2025-07-20 15:36:51 -0300

    ex18 melhorado

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,54 +1,70 @@
-let tab = document.getElementById('itab')
+let tab = document.querySelector('select#itab')
 let list = [] //array
+let res = document.getElementById('res') 
 
 function adicionar(){
-    let num = Number.parseInt(document.getElementById('inum').value)
-    
+    let caixa = document.getElementById('inum')
+    let num = Number(caixa.value)
     let msg = document.createElement('option')
 
-    if (num < 1 || num > 100 || document.getElementById('inum').value.length == 0){
-        window.alert('Número inválido ou inexistente')
+    if (!isNumber(num) || inLista(num, list)){
+        window.alert('Número inválido, inexistente ou já está na lista')
     } else {
         msg.innerHTML = `O número ${num} foi adicionado!`
-        tab.appendChild(msg)
-        vet(num) //chama a função que coloca o número na array 
+        tab.appendChild(msg) //Resposta visual 
+        list.push(num) //Adiciona o número na Array
+        res.innerHTML = ''
     }
+
+    caixa.value = '' //Limpa a caixa
+    caixa.focus() //Volta o foco do mouse para a caixa
 }
 
-let vet = function vet(num){ //função que coloca o número na array
-    list.push(num)
-    return list
+function isNumber(n){
+    if (n >= 1 && n <= 100){ //Verifica se é um número válido
+        return true
+    } else {return false}
 }
+
+function inLista(n, l){
+    if (l.indexOf(n) != -1){  //Se não tiver o valor na lista, retorna -1
+        return true 
+    } else {return false}
+}   
 
 function finalizar(){ //função de quando clica no botão "finalizar"
-    let res = document.getElementById('res')
-    
     if (list.length == 0){
         window.alert('Informe ao menos um número')
     } else{
-    list.sort()
-
-
-    res.innerHTML = `Ao todo, temos ${list.length} números cadastrados <br>
-    O maior valor informado foi ${list[list.length - 1]} <br>
-    O menor valor informado foi ${list[0]} <br>
-    A soma de todos os valores é ${soma(list)} <br>
-    A média de todos os valores é ${soma(list) / list.length}
-    ` 
+    let maior = list[0]
+    let menor = list[0]
+    let soma = 0
+    let media = 0
+    for(pos in list){
+        soma += list[pos]
+        
+        if (list[pos] > maior){
+            maior = list[pos]
+        }
+        if (list[pos] < menor){
+            menor = list[pos]
+        }
+    }
     
-    tab.innerHTML = '<option> Digite números acima! </option>'
-    list = []
+    media = soma / list.length
+
+
+    res.innerHTML = `Ao todo, temos ${list.length} números cadastrados <br> 
+    O maior valor informado foi ${maior} <br>
+    O menor valor informado foi ${menor} <br>
+    A soma de todos os valores é ${soma} <br>
+    A média de todos os valores é ${media}
+    ` 
     }
 }
 
-let soma = function soma(n){ 
-    if (n.length == 1){
-        return n
-    } else{
-        let total = 0
-        for (c in n){
-            total += n[c] //adiciona o valor em cada posição a cada loop até acabar 
-        }
-        return total
-    }
+function resetar(){
+    tab.innerHTML = '<option> Digite números acima! </option>'
+    res.innerHTML = ''
+    list = [] //Reseta tudo
 }
